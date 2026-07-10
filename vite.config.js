46bbdc447b27b68ad16574377dfb45352de99d4f
golanakonda harish <harishofficial855@gmail.com>
2026-07-10 23:48:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'Client',
-  base: '/E-Commerce/',
   plugins: [react()],
+  base: "/E-Commerce/",
   server: {
     port: 3000,
-    open: false,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
